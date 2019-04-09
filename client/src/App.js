1919f2995cce48f33f6c1edd15e0901da60da271
t4sk <tsk.nakamura@gmail.com>
2019-04-09 16:42:35 +0900

    connect websocket on client

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react"
+import React, { Component, useEffect } from "react"
 import "./App.css"
 import WindowSize from "./WindowSize"
 import Graph from "./Graph"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <WindowSize>{props => <Graph {...props} />}</WindowSize>
-      </div>
-    )
-  }
+function App(props) {
+  useEffect(() => {
+    const wsClient = new WebSocket(process.env.REACT_APP_WEB_SOCKET)
+
+    wsClient.onopen = e => console.log("web socket open", e)
+    wsClient.onmessage = e => console.log("web socket message", e)
+    wsClient.onerror = e => console.error("web socket error", e)
+    wsClient.onclose = e => console.error("web socket close", e)
+
+    return () => wsClient.close()
+  })
+
+  return (
+    <div className="App">
+      <WindowSize>{props => <Graph {...props} />}</WindowSize>
+    </div>
+  )
 }
 
 export default App
